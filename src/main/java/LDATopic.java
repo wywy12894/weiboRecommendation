f7ab3a2109beb2b0aa23796402d598c0438c98cf
wywy12894 <wangyue0502@pku.edu.cn>
2020-12-04 14:11:12 +0800

    LDA on Spark

---
+++ src/main/java/LDATopic.java
@@ -0,0 +1,52 @@
+import org.apache.spark.ml.clustering.LDA;
+import org.apache.spark.ml.clustering.LDAModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.IOException;
+
+/*
+    把微博文档转换成topicDistribution
+ */
+public class LDATopic {
+    public static void main(String[] args) {
+        // Creates a SparkSession
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("LDATopic")
+                .getOrCreate();
+
+        // Loads data.
+        Dataset<Row> dataset = spark.read().format("libsvm")
+                .load("hdfs://data/contentWithNum.txt");
+
+        // Trains a LDA model.
+        LDA lda = new LDA().setK(100).setMaxIter(100);
+        LDAModel model = lda.fit(dataset);
+
+        double ll = model.logLikelihood(dataset);
+        double lp = model.logPerplexity(dataset);
+        System.out.println("The lower bound on the log likelihood of the entire corpus: " + ll);
+        System.out.println("The upper bound on perplexity: " + lp);
+
+        // Describe topics.
+        Dataset<Row> topics = model.describeTopics(10);
+        System.out.println("The topics described by their top-weighted terms:");
+        topics.show();
+
+        // Shows the result.
+        Dataset<Row> transformed = model.transform(dataset);
+        transformed.drop("features");
+        transformed.show();
+
+        try {
+            model.write().overwrite().save("hdfs://model/LDAModel");
+            transformed.write().parquet("hdfs://model/docRepresentation.parquet");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        spark.stop();
+    }
+}
