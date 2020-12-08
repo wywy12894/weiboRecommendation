77383397e1f0be4fc208ea48535dd2344a1e6aad
wywy12894 <wangyue0502@pku.edu.cn>
2020-12-08 13:31:08 +0800

    test

--- src/main/java/InfluentialUserRecommendation.java
+++ src/main/java/InfluentialUserRecommendation.java
@@ -19,7 +19,7 @@ public class InfluentialUserRecommendation {
                 .getOrCreate();
 
 //        String filepath6 = "hdfs://hadoop-node1:9000/data/followers.txt";
-        String filepath6 = "/usr/project/SimpleProject/data/followers.txt";
+        String filepath6 = "/usr/project/data/followers.txt";
 
         JavaRDD<Edge<String>> edgeJavaRDD = spark.read()
                 .textFile(filepath6)
